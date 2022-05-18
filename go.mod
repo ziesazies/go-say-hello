f1bd3529996784212fbc5cf67ad037769eb99d65
Alief Ahmad Azies <33364922+aliefazies@users.noreply.github.com>
2022-05-18 21:12:57 +0700

    major update say hello with parameter name

--- go.mod
+++ go.mod
@@ -1,3 +1,3 @@
-module github.com/aliefazies/go-say-hello
+module github.com/aliefazies/go-say-hello/v2
 
 go 1.18
--- say_hello.go
+++ say_hello.go
@@ -1,7 +1,7 @@
 package go_say_hello
 
-func SayHello() string {
-	return "Hello World"
+func SayHello(name string) string {
+	return "Hello " + name
 }
 
 func SayHi() string {
